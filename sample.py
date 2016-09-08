512ef1d0c7560999b8404618c855996354b7f74f
rootming <rootming@live.cn>
2016-09-08 22:42:37 +0800

    add base64 decode/encode

--- sample.py
+++ sample.py
@@ -2,14 +2,42 @@
 # -*- coding: utf-8 -*-
 
 from qqbot import QQBot
+import re
+import base64
+
+base64Pattern = re.compile(r'[a-zA-Z0-9+/]+')
+
+
+def codeCheck(pattern, text):
+    match = pattern.match(text)
+    if match == None:
+        return False
+    else:
+        return True
+
+def decodeBase64(text):
+    return  base64.decodestring(text)
+
+def encodeBase64(text):
+    return base64.encodestring(text)
+    
+    
 
 class MyQQBot(QQBot):
     def onPollComplete(self, msgType, from_uin, buddy_uin, message):
+        
         if message == '-hello':
             self.send(msgType, from_uin, '你好，我是QQ机器人')
         elif message == '-stop':
             self.stopped = True
             self.send(msgType, from_uin, 'QQ机器人已关闭')
+        elif message[-2:] == '==':
+            if codeCheck(base64Pattern, message):
+                reply = 'Base64 decode result: ' + decodeBase64(message)
+                self.send(msgType, from_uin, reply)
+        elif message[:13] == 'encodeBase64:':
+            reply = 'Base64 encode result: ' + encodeBase64(message[13:])
+            self.send(msgType, from_uin, reply)
 
 myqqbot = MyQQBot()
 myqqbot.Login()
